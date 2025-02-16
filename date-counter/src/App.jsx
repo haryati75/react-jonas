4b6cecfc96479b89d8e42916092800fcef17756b
Haryati H <haryati75@gmail.com>
2025-02-16 16:28:54 +0800

    initialize date-counter app with Vite and React setup

---
+++ date-counter/src/App.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+
+export default function App() {
+  const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
+
+  return (
+    <>
+      <Step step={step} setStep={setStep} />
+      <Counter count={count} step={step} setCount={setCount} />
+      <Display count={count} />
+    </>
+  );
+}
+
+function Step({ step, setStep }) {
+  const increment = () => setStep((curStep) => curStep + 1);
+  const decrement = () => setStep((curStep) => curStep - 1);
+  return (
+    <div>
+      <button onClick={decrement}>-</button>
+      <span>Step: {step}</span>
+      <button onClick={increment}>+</button>
+    </div>
+  );
+}
+
+function Counter({ count, step, setCount }) {
+  const increment = () => setCount((curCount) => curCount + step);
+  const decrement = () => setCount((curCount) => curCount - step);
+  return (
+    <div>
+      <button onClick={decrement}>-</button>
+      <span>Count: {count}</span>
+      <button onClick={increment}>+</button>
+    </div>
+  );
+}
+
+function Display({ count }) {
+  const today = new Date();
+  const newDate = new Date(today);
+  newDate.setDate(today.getDate() + count);
+
+  return (
+    <p>
+      {count === 0 && `Today is ${today.toDateString()}`}
+      {count > 0 && `In ${count} days from today is ${newDate.toDateString()}`}
+      {count < 0 && `${Math.abs(count)} days ago was ${newDate.toDateString()}`}
+    </p>
+  );
+}
