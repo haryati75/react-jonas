209bfb1c307a3005ce0043e48aec84d7b370a2fa
Haryati H <haryati75@gmail.com>
2025-02-18 18:51:57 +0800

    enhance Stats component to display item count and packing percentage

--- travel-list/src/App.jsx
+++ travel-list/src/App.jsx
@@ -34,7 +34,7 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -137,10 +137,28 @@ function Item({
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  const numItems = items.length;
+
+  if (numItems === 0) {
+    return (
+      <footer className="stats">
+        📝 Start adding some items to your packing list
+      </footer>
+    );
+  }
+
+  const numPackedItems = items.filter((item) => item.packed).length;
+  const percentPacked = (numPackedItems / numItems) * 100;
+  const formattedPercentPacked = Math.round(percentPacked) || 0;
+
   return (
     <footer className="stats">
-      <em>📸 You have X items on your list, and you already packed X (X%)</em>
+      <em>
+        {percentPacked === 100
+          ? "🎉 All Packed! Ready to go 🛩️"
+          : `📸 You have ${numItems} items on your list, and you already packed ${numPackedItems} (${formattedPercentPacked}%)`}
+      </em>
     </footer>
   );
 }
