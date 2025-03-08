ae1d4dd37b7f491f1bc3e77cf4b22871af924dcc
Haryati H <haryati75@gmail.com>
2025-03-08 23:46:11 +0800

    feat: add Progress component to display quiz progress and scoring

--- react-quiz/src/App.jsx
+++ react-quiz/src/App.jsx
@@ -6,6 +6,7 @@ import Error from "./components/Error";
 import StartScreen from "./components/StartScreen";
 import Question from "./components/Question";
 import NextButton from "./components/NextButton";
+import Progress from "./components/Progress";
 
 const initialState = {
   questions: [],
@@ -58,6 +59,10 @@ export default function App() {
   const { questions, status, index, answer, points } = state;
 
   const numQuestions = questions.length;
+  const maximumPoints = questions.reduce(
+    (acc, question) => acc + question.points,
+    0
+  );
 
   useEffect(() => {
     fetch("http://localhost:4000/questions")
@@ -77,6 +82,13 @@ export default function App() {
         )}
         {status === "active" && (
           <>
+            <Progress
+              index={index}
+              numQuestions={numQuestions}
+              points={points}
+              maximumPoints={maximumPoints}
+              answer={answer}
+            />
             <Question
               question={questions.at(index)}
               dispatch={dispatch}
