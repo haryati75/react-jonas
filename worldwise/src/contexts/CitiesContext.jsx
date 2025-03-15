bfe295ae97014211c51837db607ca88a91e374a2
Haryati H <haryati75@gmail.com>
2025-03-16 00:02:46 +0800

    feat: implement CitiesContext for managing city data and loading state

---
+++ worldwise/src/contexts/CitiesContext.jsx
@@ -0,0 +1,42 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+const CitiesContext = createContext();
+
+function CitiesProvider({ children }) {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const URL = "http://localhost:8000/cities";
+
+  useEffect(() => {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(URL);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert("There was an error fetching the data");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
+  return (
+    <CitiesContext.Provider value={{ cities, isLoading }}>
+      {children}
+    </CitiesContext.Provider>
+  );
+}
+
+function useCities() {
+  const context = useContext(CitiesContext);
+  if (context === undefined) {
+    throw new Error("useCities must be used within a CitiesProvider");
+  }
+  return context;
+}
+
+export { CitiesProvider, useCities };
