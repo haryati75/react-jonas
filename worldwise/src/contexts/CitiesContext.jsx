1647b4fdc1ce7da476005abf3deab7448e309b55
Haryati H <haryati75@gmail.com>
2025-03-16 18:48:38 +0800

    fix: ensure currentCity ID comparison is type-safe in getCity function

--- worldwise/src/contexts/CitiesContext.jsx
+++ worldwise/src/contexts/CitiesContext.jsx
@@ -64,7 +64,7 @@ function CitiesProvider({ children }) {
   }, []);
 
   async function getCity(id) {
-    if (id === currentCity.id) return;
+    if (Number(id) === currentCity.id) return;
 
     dispatch({ type: "loading" });
     try {
