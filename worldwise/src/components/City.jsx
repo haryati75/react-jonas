db53e68ddf98d5ff2756129e4a2ecc5d0dd91b63
Haryati H <haryati75@gmail.com>
2025-03-16 12:53:50 +0800

    feat: integrate Leaflet for map functionality, update city data handling, and clean up components

--- worldwise/src/components/City.jsx
+++ worldwise/src/components/City.jsx
@@ -1,7 +1,7 @@
 import { useParams } from "react-router-dom";
-import styles from "./City.module.css";
-import { useCities } from "../contexts/CitiesContext";
 import { useEffect } from "react";
+import { useCities } from "../contexts/CitiesContext";
+import styles from "./City.module.css";
 import Spinner from "./Spinner";
 import BackButton from "./BackButton";
 
