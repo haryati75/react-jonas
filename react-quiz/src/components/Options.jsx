0ac8b01fde38d5d47b074d01fd414a3b87dc0703
Haryati H <haryati75@gmail.com>
2025-03-22 08:50:20 +0800

    refactor: simplify Options and Question components to use QuizContext for state management

--- react-quiz/src/components/Options.jsx
+++ react-quiz/src/components/Options.jsx
@@ -1,4 +1,7 @@
-export default function Options({ question, dispatch, answer }) {
+import { useQuiz } from "../context/QuizContext";
+
+export default function Options({ question }) {
+  const { answer, dispatch } = useQuiz();
   const hasAnswered = answer !== null;
 
   function handleOnClick(answerIndex) {
--- react-quiz/src/components/Question.jsx
+++ react-quiz/src/components/Question.jsx
@@ -2,12 +2,12 @@ import { useQuiz } from "../context/QuizContext";
 import Options from "./Options";
 
 export default function Question() {
-  const { questions, index, answer, dispatch } = useQuiz();
+  const { questions, index } = useQuiz();
   const question = questions.at(index);
   return (
     <div>
       <h4>{question.question}</h4>
-      <Options question={question} dispatch={dispatch} answer={answer} />
+      <Options question={question} />
     </div>
   );
 }
