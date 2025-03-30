1e77980e43125b2064a18f85ffb13611488bda13
Haryati H <haryati75@gmail.com>
2025-03-30 23:37:55 +0800

    feat: integrate react-router for navigation and add AppLayout component

---
+++ fast-react-pizza/src/ui/Header.jsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router";
+
+export default function Header() {
+  return (
+    <header>
+      <Link to="/">Fast React Pizza Co.</Link>
+      <p>Haryati</p>
+    </header>
+  );
+}
