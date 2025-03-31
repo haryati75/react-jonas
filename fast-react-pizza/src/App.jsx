a6405745f5c51e4a8da561d6e33bf98e8c9ca733
Haryati H <haryati75@gmail.com>
2025-03-31 09:48:05 +0800

    feat: implement order creation and retrieval functionality with form handling and data loading

--- fast-react-pizza/src/App.jsx
+++ fast-react-pizza/src/App.jsx
@@ -5,8 +5,10 @@ import Home from "./ui/Home";
 import Error from "./ui/Error";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
-import CreateOrder from "./features/order/CreateOrder";
-import Order from "./features/order/Order";
+import CreateOrder, {
+  action as createOrderAction,
+} from "./features/order/CreateOrder";
+import Order, { loader as orderLoader } from "./features/order/Order";
 import AppLayout from "./ui/AppLayout";
 
 const router = createBrowserRouter([
@@ -31,10 +33,14 @@ const router = createBrowserRouter([
       {
         path: "order/new",
         element: <CreateOrder />,
+        action: createOrderAction,
+        errorElement: <Error />,
       },
       {
         path: "order/:orderId",
         element: <Order />,
+        loader: orderLoader,
+        errorElement: <Error />,
       },
     ],
   },
